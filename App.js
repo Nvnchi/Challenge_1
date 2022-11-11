b83902c63c241a5031d25a27bd56129874448684
Nvnchi <r0879363@student.thomasmore.be>
2022-11-11 17:31:31 +0100

    Styling + lijntjes + dark mode = opdracht klaar

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import Book from './components/Book'
@@ -7,62 +7,80 @@ import Book from './components/Book'
 export default function App() {
 
   return (
-    <View style={styles.container}>
-      <View style={styles.title}>
-        <Text>Bookshop</Text>
+    <ScrollView style={styles.container}>
+      <View style={styles.header}>
+        <View>
+          <Text style={styles.title}>Bookshop</Text>
+        </View>
+
+        <View style={styles.shoppingcart}>
+          <FontAwesomeIcon icon={faShoppingCart} size={30} color={"white"} />
+          <Text style={styles.shoppingcart_text}>1</Text>
+        </View>
       </View>
 
-      <View style={styles.shoppingcart}>
-        <FontAwesomeIcon icon={faShoppingCart} size={40} color={"black"} />
-        <Text>1</Text>
-      </View>
-
-      <Book 
+      <Book
         title="Book title 1"
-        author="by Some Random dude"
+        author="Some Random dude"
         text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer luctus, nulla sit amet pellentesque ultricies, magna nsl vulputate urna, quis feugiat puru enim id ex."
         isbn="ISBN 978-8-2165-1860-1"
       />
       <Book 
         title="Book title 2"
-        author="by some other dude"
+        author="some other dude"
         text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer luctus, nulla sit amet pellentesque ultricies, magna nsl vulputate urna, quis feugiat puru enim id ex."
         isbn="ISBN 978-2-1425-4277-8"
       />
       <Book 
         title="Book title 3"
-        author="by Jane Doe"
+        author="Jane Doe"
         text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer luctus, nulla sit amet pellentesque ultricies, magna nsl vulputate urna, quis feugiat puru enim id ex."
         isbn="ISBN 978-5-9684-0436-7"
       />
       <Book 
         title="Book title 4"
-        author="by John Doe"
+        author="John Doe"
         text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer luctus, nulla sit amet pellentesque ultricies, magna nsl vulputate urna, quis feugiat puru enim id ex."
         isbn="ISBN-978-8-3356-4834-0"
       />
       <StatusBar style="auto" />
-    </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   title: {
-    fontSize: 60,
+    fontSize: 30,
     fontWeight: 'bold',
+    color: '#0096FF',
   },
-  book: {
-    backgroundColor: '#fff',
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 40,
+    marginTop: 50,
+  },
+  shoppingcart: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  shoppingcart_text: {
+    position: 'absolute',
+    top: 22,
+    right: -5,
+    fontSize: 10,
+    fontWeight: 'bold',
+    color: 'white',
   },
   image: {
     width: 50,
     height: 50,
-    color: 'black',
+    color: 'white',
   },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     padding: 20,
-    marginTop: 30,
+    backgroundColor: '#000',
   },
 });
--- components/Book.js
+++ components/Book.js
@@ -1,30 +1,83 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
 
 const Book = (props) => {
 
-  const Read = (title) => {
-    console.log(title + ' read');
+  const Read = () => {
+    console.log(props.title + ' read');
   };
 
-  const Pressed = (title) => {
-    console.log(title + ' pressed');
+  const Pressed = () => {
+    console.log(props.title + ' pressed');
   };
 
   return (
     <View style={styles.book}>
-        <Text onPress={() => Pressed(props.title)}>{props.title}</Text>
-        <Text>by {props.author}</Text>
-        <Text>{props.text}</Text>
-        <Text>{props.isbn}</Text>
-        <Button onPress={() => Read(props.title)} title="Read"/>
+      <Text style={styles.book_title} onPress={() => Pressed()}>{props.title}</Text>
+      <Text style={styles.book_author}>by {props.author}</Text>
+      <Text style={styles.book_text}>{props.text}</Text>
+      <View style={styles.bottom}>
+        <Text style={styles.book_isbn}>{props.isbn}</Text>
+        <Pressable style={styles.button} onPress={() => Read()}>
+          <Text style={styles.button_text}>Read</Text>
+        </Pressable>
+      </View>
+      <View
+        style={{
+          marginTop: 20,
+          borderBottomColor: 'white',
+          borderBottomWidth: StyleSheet.hairlineWidth,
+        }}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
     book: {
-      backgroundColor: '#fff',
+      flexDirection: 'column',
+      marginBottom: 30,
+    },
+    book_title: {
+      fontSize: 20,
+      fontWeight: 'bold',
+      marginBottom: 5,
+      color: '#F637EC',
+    },
+    book_author: {
+      fontSize: 10,
+      fontStyle: 'italic',
+      marginBottom: 5,
+      color: '#FBB454',
+      fontWeight: 'bold',
+    },
+    book_isbn: {
+      fontSize: 10,
+      color: 'white',
+    },
+    book_text: {
+      marginBottom: 5,
+      color: '#FAEA48',
+    },
+    bottom: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+    },
+    button: {
+      fontSize: 20,
+      fontWeight: 'bold',
+      backgroundColor: 'white',
+      paddingVertical: 8,
+      paddingHorizontal: 8,
+      borderRadius: 4,
+      width: 60,
+    },
+    button_text: {
+      color: 'black',
+      fontWeight: 'bold',
+      fontSize: 16,
+      textAlign: 'center',
     },
 });
 
